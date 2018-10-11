f8e973fa796b922ac6954a3e85ff7908466a5107
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-11 14:21:05 +0200

    use generic-model-utils to render card

--- addon/components/editor-plugins/mandataris-card.js
+++ addon/components/editor-plugins/mandataris-card.js
@@ -1,7 +1,29 @@
 import Component from '@ember/component';
 import BaseCardComponent from '../../mixins/base-card-component';
 import layout from '../../templates/components/editor-plugins/mandataris-card';
+import ResourceRelationCardMixin from '@lblod/ember-generic-model-plugin-utils/mixins/resource-relation-card-mixin';
 
-export default Component.extend(BaseCardComponent, {
-  layout
+export default Component.extend(ResourceRelationCardMixin, {
+  layout,
+
+  serializeToJsonApi(resource){
+    //This is because we're not sure uri is kept (due to bug in mu-cl-resources/or ember-ds?)
+    let serializedResource = resource.serialize({includeId: true});
+    serializedResource.data.attributes.uri = resource.uri;
+    return serializedResource;
+  },
+
+  actions: {
+    async refer(data){
+      this.refer(await data.rdfaProperty,
+                 this.serializeToJsonApi(data.mandataris),
+                 data.mandataris.isBestuurlijkeAliasVan.get('fullName'),
+                 'editor-plugins/mandataris-card');
+    },
+    async extend(data){
+      await this.extend(await data.rdfaProperty,
+                        this.serializeToJsonApi(data.mandataris),
+                        'editor-plugins/mandataris-card');
+    }
+  }
 });
