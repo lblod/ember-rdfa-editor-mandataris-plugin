c4956baec24caa52ae0c59b3a46c5d7882319b75
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-11 15:46:38 +0200

    additional model

--- addon/models/custom-inflector-rules.js
+++ addon/models/custom-inflector-rules.js
@@ -6,6 +6,7 @@ inflector.irregular("mandataris","mandatarissen");
 inflector.irregular("mandaat","mandaten");
 inflector.irregular("persoon","personen");
 inflector.irregular("bestuursfunctie-code","bestuursfunctie-codes");
+inflector.irregular("bestuurseenheid","bestuurseenheden");
 
 // Meet Ember Inspector's expectation of an export
 export default {};
